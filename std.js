2e14216dbb3f9190866415cfd25e6513f1e9e526
easrng <easrng@gmail.com>
2021-03-10 11:04:22 -0500

    fix bug

--- std.js
+++ std.js
@@ -1,8 +1,8 @@
 module.exports={
   log({arguments}){
     console.log(arguments[0])
-  }
-  if({arguments, blocks}){
+  },
+  if:({arguments, blocks})=>{
     if(arguments[0]){} // TODO: Implement this!
   }
 }
